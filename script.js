4f33167c668aa479dd19a00988f969109089d7e5
Leroy Douglas <leroi.douglas@gmail.com>
2020-10-08 02:42:45 +0100

    Update script.js
    Added Documentation

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ const displayFooter = _ => {
 };
 
 const setup = _ => {
-  new Shows().addShowsContent(); // create shows data for shows & update view
-  new Episodes(); // At this time we only need to set up the episodes object...no data required right
-  // now
-  new UIPanel().createUIPanel(); // create all UI components - imput selectors for shows & episodes
-  //                                          - search bar
-  //                                          - shows view button - Allows user to return to shows
-  //                                             view
-  //                                          - counter to show how many shows / episodes
+  new Shows().addShowsContent(); // create shows data for all shows (Shows.js) & update view
+  new Episodes();                             // At this time we only need to set up the episodes (Episodes.js) object...
+                                                         // no data required as we only need to view the available shows
+  new UIPanel().createUIPanel();    // create all UI components (UIPanel.js)
+                                                         // - imput selectors for shows (ShowSelect.js) & episodes (EpisodeSelect.js)
+                                                         // - search bar (Search.js)
+                                                         // - shows view button (UIPanel.js) - Allows user to return to shows view
+                                                         // - counter (MovieCounter.js) to show how many shows / episodes are being displayed
   displayFooter();
 };
 
